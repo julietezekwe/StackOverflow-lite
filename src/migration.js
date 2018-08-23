87a2c43d836caf69428bad7ba3476434acb085ca
odunayo <odunayo.ogungbure@ss-limited.com>
2018-08-23 08:00:47 +0100

    feat(database): fetch questions from database - fetch all questions from database
    [Finishes #159958337]

--- src/migration.js
+++ src/migration.js
@@ -1,5 +1,5 @@
 import pool from './dbconnect';
 
 pool.query('CREATE TABLE IF NOT EXISTS Users(id SERIAL PRIMARY KEY, name VARCHAR(100) not null,  email VARCHAR(100) not null, password VARCHAR(100) not null)');
-pool.query('CREATE TABLE IF NOT EXISTS Questions(id SERIAL PRIMARY KEY, title VARCHAR(100) not null, context TEXT not null, user_id INTEGER REFERENCES users)');
+pool.query('CREATE TABLE IF NOT EXISTS Questions(id SERIAL PRIMARY KEY, title VARCHAR(100) not null, context TEXT not null, user_id INTEGER not null)');
 pool.query('CREATE TABLE IF NOT EXISTS Answers(id SERIAL PRIMARY KEY, answer TEXT not null, question_id INTEGER not null)');

90ce4dbe84c11dc321c1bb2bf453b2db12bc34a4
odunayo <odunayo.ogungbure@ss-limited.com>
2018-08-23 08:29:43 +0100

    feat(database): fetch questions from database - fetch all questions from database
    [Finishes #159958337]

--- test/api.js
+++ test/api.js
@@ -5,58 +5,6 @@ import app from '../dist/app';
 const should = chai.should();
 
 describe('Test api requests', () => {
-  describe('Test question routes', () => {
-    describe('Fetch question', () => {
-      it('respond with json containing list of all questions', () => {
-        request(app).get('/api/v1/questions')
-          .end((err, res) => {
-            res.should.have.property('status', 200);
-            res.body.should.be.a('array');
-            res.body[0].should.have.all.keys('id', 'title', 'context', 'user_id');
-          });
-      });
-    });
-    describe('Add a question', () => {
-      it('respond with json containing question added', () => {
-        request(app).post('/api/v1/questions')
-          .send({
-            title: 'Mocha Test Question',
-            context: 'Mocha Test Body of Question Ok!',
-          })
-          .end((err, res) => {
-            res.should.have.property('status', 201);
-            res.body.should.be.a('object');
-            res.body.should.have.all.keys('id', 'title', 'context', 'user_id');
-            res.body.title.should.equal('Mocha Test Question');
-            res.body.context.should.equal('Mocha Test Body of Question Ok!');
-          });
-      });
-      it('respond with invalid request for empty body data', () => {
-        request(app).post('/api/v1/questions')
-          .send({
-            title: '',
-            context: '',
-          })
-          .end((err, res) => {
-            res.should.have.property('status', 400);
-            res.body.should.be.a('object');
-            res.body.should.have.all.keys('error');
-          });
-      });
-      it('respond with invalid request for bad body data', () => {
-        request(app).post('/api/v1/questions')
-          .send({
-            foo: 'Lorem Ipsum',
-            bar: 'Grace ipsum',
-          })
-          .end((err, res) => {
-            res.should.have.property('status', 400);
-            res.body.should.be.a('object');
-            res.body.should.have.all.keys('error');
-          });
-      });
-    });
-  });
   describe('Test invalid route', () => {
     it('respond with route not found', () => {
       request(app).get('/api/v9878/facebook')
